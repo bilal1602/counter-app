411db29439269ded4848ec95a9729d5d04472e08
Bilal <bilal.sajjad786@gmail.com>
2020-08-08 19:52:25 +0500

    State lifted up to App component.

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+      { id: 5, value: 0 },
+    ],
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = this.state.counters.indexOf(counter);
+    counter.value++;
+    counters[index] = counter;
+    this.setState({ counters });
+  };
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(
+      (counter) => counter.id !== counterId
+    );
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="Container">
+          <Counters
+            counters={this.state.counters}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onReset={this.handleReset}
+          />
+        </main>
+      </React.Fragment >
+    );
+  }
+}
+
+export default App;
